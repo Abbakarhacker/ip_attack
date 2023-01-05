5c34dde8a3f2cc65d212be21c70ca71a0918d9b3
Abbakarhacker <110747527+Abbakarhacker@users.noreply.github.com>
2023-01-05 15:22:50 +0100

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,57 @@
+#!/bin/python
+
+# 
+
+#
+
+# Name	:	IP Attack
+
+# Code		:	python
+
+# Sec.Code	:	8h4i
+
+# Coder		: 	Abbakareo
+
+# Site		: 	arewaanonymous.blogspot.com
+
+# github	:	github.com/Abbakarhacker
+
+#
+
+#
+
+#	<==(__Warning__)==>
+
+#
+
+# DO NOT MODIFY OR COPY WITHOUT PERMISSION !!!
+
+#
+
+#	<==(__WARNING__)==>
+
+#
+
+#
+
+#
+
+apt update
+
+apt upgrade 
+
+apt install figlet -y
+
+apt install toilet -y
+
+apt install nano -y
+
+apt install ruby -y
+
+apt install curl -y
+
+gem install lolcat
+
+clear
+
+toilet -f pagga "Done..!!" | lolcat
